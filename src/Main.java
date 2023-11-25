45dfe5cc73e866a28b82151545128551cfa69eb7
playedead <awwii94@gmail.com>
2023-11-25 19:06:12 +0400

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,22 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(String[] args) {
-
+    int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int[] destination = getNewArray(arr, 3, 7);
+    System.out.println(Arrays.toString(destination));
+    int[] destination1 = getNewArray(arr, 0, 7);
+    System.out.println(Arrays.toString(destination1));
+  }
+  private static int[] getNewArray(int[] arr, int start, int end) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 == 0) {
+        arr[i]++;
+      }
+    }
+    int destinationLength = end - start + 1;
+    int[] destination = new int[destinationLength];
+    System.arraycopy(arr, start, destination, 0, destinationLength);
+    return destination;
   }
 }
 
