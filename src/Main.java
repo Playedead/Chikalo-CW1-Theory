7b92002a651ca3a4f5651c15e2db78842d4f5f0b
playedead <awwii94@gmail.com>
2023-11-25 19:07:39 +0400

    theory1

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
     int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     int[] destination = getNewArray(arr, 3, 7);
     System.out.println(Arrays.toString(destination));
-    int[] destination2 = getNewArray(arr, 0, 7);
-    System.out.println(Arrays.toString(destination2));
+    int[] destination1 = getNewArray(arr, 0, 7);
+    System.out.println(Arrays.toString(destination1));
   }
   private static int[] getNewArray(int[] arr, int start, int end) {
     for (int i = 0; i < arr.length; i++) {
